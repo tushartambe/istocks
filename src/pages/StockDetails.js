fb4c206022b0a4101fb3b51f2598bc6b14ee3a40
Tushar Tambe <44019295+tushartambe@users.noreply.github.com>
2021-03-16 19:02:27 +0530

    Add colored indicating bars for showing high and low

--- src/pages/StockDetails.js
+++ src/pages/StockDetails.js
@@ -1,10 +1,11 @@
-import { HeartOutlined } from '@ant-design/icons';
-import { Avatar, Button, Card, Col, InputNumber, Row, Space, Tabs, Typography } from 'antd';
+import { HeartOutlined, FrownOutlined, SmileOutlined } from '@ant-design/icons';
+import { Avatar, Button, Card, Col, Tooltip, Slider, InputNumber, Divider, Row, Tabs, Typography, Space } from 'antd';
 import React from 'react';
 import CustomLayout from '../components/CustomLayout';
 import CustomPropertyText from '../components/CustomPropertyText';
 import { INR, NSE_QUOTE_URL } from "../constants/constants";
 import { getAvatarText } from '../utils/utils';
+import './StockDetails.css';
 
 const { Title, Link, Text } = Typography;
 const { Meta } = Card;
@@ -23,6 +24,14 @@ const StockDetails = (props) => {
     yearHigh: 10200
   }
 
+  const tipFormatter = (value) => {
+    return (
+      <div>
+        {INR + stockInfo.currentPrice}
+      </div>
+    )
+  }
+
   return (
     <CustomLayout>
       <div style={{ marginRight: '25%', marginLeft: '25%', marginTop: '10px' }}>
@@ -34,18 +43,64 @@ const StockDetails = (props) => {
             title={<Title level={4}>{stockInfo.name}</Title>}
             description={<Title level={5}>{INR}{stockInfo.currentPrice}</Title>}
           />
-          <Row gutter={16} >
-            <Col key="open-price" span={8}>
-              <CustomPropertyText name="Open Price" value={stockInfo.openPrice} prefix={INR}></CustomPropertyText>
+          <br />
+          <Title level={5}>
+            Performance
+            </Title>
+          <Row wrap={false} gutter={24, 48}>
+            <Col flex="none">
+              <div style={{}}>
+                <CustomPropertyText name="Today's Low" value={stockInfo.dayLow} prefix={INR}></CustomPropertyText>
+              </div>
             </Col>
-            <Col key="close-price" span={8}>
-              <CustomPropertyText name="Previous Close" value={stockInfo.previousClose} prefix={INR}></CustomPropertyText>
+            <Col flex="auto" style={{ pointerEvents: 'none' }}>
+              <Slider
+                included={false}
+                defaultValue={100 - ((stockInfo.dayHigh - stockInfo.currentPrice) / (stockInfo.dayHigh - stockInfo.dayLow)) * 100}
+                tooltipVisible={true}
+                tipFormatter={tipFormatter}
+                handleStyle={{ borderColor: '#695AAF' }}
+              />
             </Col>
-            <Col key="day-low" span={8}>
-              <CustomPropertyText name="Today's Low" value={stockInfo.dayLow} prefix={INR}></CustomPropertyText>
+            <Col flex="none">
+              <div style={{}}>
+                <CustomPropertyText name="Today's High" value={stockInfo.dayHigh} prefix={INR}></CustomPropertyText>
+              </div>
             </Col>
           </Row>
-          <Row gutter={16} >
+          <Row wrap={false} gutter={24, 48}>
+            <Col flex="none">
+              <div style={{}}>
+                <CustomPropertyText name="52 week Low" value={stockInfo.yearLow} prefix={INR}></CustomPropertyText>
+              </div>
+            </Col>
+            <Col flex="auto" style={{ pointerEvents: 'none' }}>
+              <Slider
+                included={false}
+                defaultValue={100 - ((stockInfo.yearHigh - stockInfo.currentPrice) / (stockInfo.yearHigh - stockInfo.yearLow)) * 100}
+                tooltipVisible={true}
+                tipFormatter={tipFormatter}
+                handleStyle={{ borderColor: '#695AAF' }}
+              />
+            </Col>
+            <Col flex="none">
+              <div style={{}}>
+                <CustomPropertyText name="52 week high" value={stockInfo.yearHigh} prefix={INR}></CustomPropertyText>
+              </div>
+            </Col>
+          </Row>
+          <Row gutter={24} justify="space-around" >
+            <Col key="open-price" span={6}>
+              <CustomPropertyText name="Open Price" value={stockInfo.openPrice} prefix={INR}></CustomPropertyText>
+            </Col>
+            <Col key="close-price" span={6}>
+              <CustomPropertyText name="Previous Close" value={stockInfo.previousClose} prefix={INR}></CustomPropertyText>
+            </Col>
+            {/* <Col key="day-low" span={8}>
+              <CustomPropertyText name="Today's Low" value={stockInfo.dayLow} prefix={INR}></CustomPropertyText>
+            </Col> */}
+          </Row>
+          {/* <Row gutter={24} >
             <Col key="day-high" span={8}>
               <CustomPropertyText name="Today's High" value={stockInfo.dayHigh} prefix={INR}></CustomPropertyText>
             </Col>
@@ -55,7 +110,7 @@ const StockDetails = (props) => {
             <Col key="52week-high" span={8}>
               <CustomPropertyText name="52 week high" value={stockInfo.yearHigh} prefix={INR}></CustomPropertyText>
             </Col>
-          </Row>
+          </Row> */}
           <Link href={NSE_QUOTE_URL + symbol} target="_blank">
             More Information..
           </Link>
